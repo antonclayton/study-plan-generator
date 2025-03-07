17332c24f558a979eb7c3aff6f9e5d9c824546de
Anton Clayton <anton.clayton@gmail.com>
2025-03-06 19:57:50 -0800

    styling: add responsive styling for CreatePlan component

--- frontend/src/components/StudyPlanPageComponents/CreatePlan.tsx
+++ frontend/src/components/StudyPlanPageComponents/CreatePlan.tsx
@@ -20,24 +20,28 @@ const CreatePlan: React.FC<CreatePlanProps> = ({ handleCreateStudyPlan }) => {
     setPlan("");
   };
   return (
-    <div className="flex flex-col justify-center items-center w-full bg-red-400 h-1/4 m-2">
+    <div className="flex flex-col justify-center items-center w-full bg-gray-700 h-1/4 m-2 rounded-xl">
+      <p className="w-2/3 text-[10px] lg:p-1 lg:m-1 sm:text-sm lg:text-base xl:text-lg text-white text-center">
+        Please be specific! <br />
+        Example: Introductory Python
+      </p>
       <input
         type="text"
         placeholder="I want to learn _____"
         value={goal}
         onChange={(e) => setGoal(e.target.value)}
-        className="border-2 border-black m-2 text-center bg-white p-2 rounded-xl"
+        className="border-2 border-black m-0.5 text-center w-3/4 bg-white p-0.5 rounded-xl text-xs lg:p-2 lg:m-2 lg:text-xl font-bold"
       />
-      <input
+      {/* <input
         type="text"
         placeholder="plan placeholder"
         value={plan}
         onChange={(e) => setPlan(e.target.value)}
         className="border-2 border-black m-2 w-3/4 bg-white p-2 rounded-xl"
-      />
+      /> */}
       <button
         onClick={handleSubmit}
-        className="border-2 bg-green-800 cursor-pointer py-2 px-4 rounded-xl m-2 text-white border-black font-bold"
+        className="border-2 bg-green-800 cursor-pointer w-3/4 text-xs py-0.5 px-1 rounded-xl m-2 text-white border-black font-bold lg:py-2 lg:px-4 lg:text-xl"
       >
         Generate Plan
       </button>
