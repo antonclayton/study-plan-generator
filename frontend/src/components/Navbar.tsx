3abc052fc3af0c586c3741792cdca3f3521b607d
Anton Clayton <anton.clayton@gmail.com>
2025-03-06 20:15:52 -0800

    styling: add hover effects to navbar elements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar = () => {
     <div className="bg-black h-16 flex justify-evenly items-center">
       <Link
         to="/"
-        className="text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105"
+        className="text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
       >
         Home
       </Link>
       <Link
         to="/plans"
-        className="text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105"
+        className="text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
       >
         Study Plans
       </Link>
