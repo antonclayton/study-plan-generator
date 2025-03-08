aa57b898e8b2c60721b54eab4e7b0601eeb29baf
Anton Clayton <anton.clayton@gmail.com>
2025-03-07 19:09:16 -0800

    styling: add horizontal line below study plan title in list component

--- frontend/src/components/StudyPlanPageComponents/StudyPlansList.tsx
+++ frontend/src/components/StudyPlanPageComponents/StudyPlansList.tsx
@@ -17,10 +17,11 @@ const StudyPlansList: React.FC<StudyPlansListProps> = ({
 }) => {
   return (
     <div className="flex flex-col items-center w-full bg-zinc-950 h-3/4 rounded-b-3xl border-x-4 border-t-2 border-b-4 border-zinc-700">
-      <div className="flex w-3/4 mt-2 sm:mt-6 lg:mt-10">
+      <div className="flex flex-col w-3/4 mt-2 sm:mt-6 lg:mt-10">
         <h2 className="font-bold text-white m-2 text-sm sm:text-base lg:text-xl xl:text-2xl">
           Study Plans:
         </h2>
+        <span className="bg-zinc-700 h-0.5 mb-2 rounded-full"></span>
       </div>
       {/* List container (overflows y)*/}
       <div className="flex flex-col justify-start items-center w-3/4 max-h-3/4 overflow-y-auto">
