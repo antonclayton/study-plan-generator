46b24c4eba23d01505fc16b36804c177688b1436
Anton Clayton <anton.clayton@gmail.com>
2025-03-07 21:02:18 -0800

    change background color for home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex bg-white justify-center items-center h-[calc(100vh-4rem)]">
-      <h1 className="font-bold text-2xl">Welcome to STUDY PLAN GENERATOR</h1>
+    <div className="flex bg-zinc-800 justify-center items-start h-[calc(100vh-4rem)]">
+      <h1 className="font-bold text-2xl text-white mt-10">
+        Welcome to STUDY PLAN GENERATOR
+      </h1>
     </div>
   );
 };
