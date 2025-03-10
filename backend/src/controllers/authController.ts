c0c6e3b17399627b218ad0fdfdbbc0652504c1ea
Anton Clayton <anton.clayton@gmail.com>
2025-03-09 17:05:08 -0700

    create file structure for JWT auth implementation and create Prisma and zod schemas for user model

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Request, Response } from "express";
+import prisma from "../prisma/client";
+
+export async function registerUser(req: Request, res: Response) {}
+export async function loginUser(req: Request, res: Response) {}
