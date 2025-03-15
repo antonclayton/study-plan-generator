51a4a4dfa8b97105230084bcef969ffcaf4979d7
Anton Clayton <anton.clayton@gmail.com>
2025-03-15 16:51:38 -0700

    feature: integrate AuthContext's useAuth to prevent access to LoginPage and StudyPage when user is logged in. Also, add same logic to navbar and have a logout button for when user is logged in instead of 'login'

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../auth/AuthProvider";
 
 const Navbar = () => {
+  const { isLoggedIn, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <div className="bg-black h-16 flex justify-evenly items-center">
       <Link
@@ -15,12 +21,21 @@ const Navbar = () => {
       >
         Study Plans
       </Link>
-      <Link
-        to="/login"
-        className="text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
-      >
-        Login
-      </Link>
+      {isLoggedIn ? (
+        <button
+          onClick={handleLogout}
+          className="bg-red-400 px-4 py-2 rounded-md  text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
+        >
+          Logout
+        </button>
+      ) : (
+        <Link
+          to="/login"
+          className="text-white text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
+        >
+          Login
+        </Link>
+      )}
     </div>
   );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../auth/AuthProvider";
 
 const Home = () => {
+  const { isLoggedIn } = useAuth();
   return (
     <div className="flex flex-col bg-zinc-800 justify-center items-center h-[calc(100vh-4rem)]">
       <h1 className="font-bold text-2xl text-white mb-10">
         Welcome to STUDY PLAN GENERATOR
       </h1>
-      <Link
-        to="/login"
-        className="text-white bg-zinc-700 p-2 text-center  w-1/5 lg:w-1/10 rounded-xl text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200 mb-10"
-      >
-        Login
-      </Link>
-      <Link
-        to="/register"
-        className="text-white bg-zinc-700 text-sm p-2 text-center w-1/5 lg:w-1/10 rounded-xl font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
-      >
-        Register
-      </Link>
+      {isLoggedIn ? (
+        <div className="font-bold text-2xl text-white mb-10">
+          Nothing Here :3
+        </div>
+      ) : (
+        <div className="flex flex-col bg-zinc-800 justify-center items-center w-full">
+          <Link
+            to="/login"
+            className="text-white bg-zinc-700 p-2 text-center  w-1/5 lg:w-1/10 rounded-xl text-sm font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200 mb-10"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="text-white bg-zinc-700 text-sm p-2 text-center w-1/5 lg:w-1/10 rounded-xl font-bold cursor-pointer sm:text-base lg:text-xl hover:underline hover:scale-105 transition-transform duration-200"
+          >
+            Register
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
